60b1082e8ccecba3b15ff106ff1b9195d69ec377
yunuchoi <yoonwoochoi00@gmail.com>
2026-04-18 14:13:40 +1200

    feat: integrate @vercel/analytics in main.tsx and remove from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ClockTimetablePage from './pages/ClockTimetablePage';
-import { Analytics } from '@vercel/analytics/next';
 
 const App = () => {
   return (
@@ -10,7 +9,6 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/clock-timetable" element={<ClockTimetablePage />} />
       </Routes>
-      <Analytics />
     </BrowserRouter>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import './fonts.css';
 import theme from './theme';
 import App from './App';
 
+import { Analytics } from "@vercel/analytics/react";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
